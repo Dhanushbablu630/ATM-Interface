78fde9afdc46708f9c38935c7d2715d575d00165
Dhanush <148046121+Dhanushbablu630@users.noreply.github.com>
2025-11-20 21:27:04 +0530

    Add Transaction class with type and amount fields

---
+++ Transaction.java
@@ -0,0 +1,17 @@
+public class Transaction {
+    private String type;
+    private int amount;
+
+    public Transaction(String type, int amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
